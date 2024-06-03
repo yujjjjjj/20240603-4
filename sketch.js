9017bd2f7594fd60605edbf2dac5ee3ba3461090
yujjjjjj <150640274+yujjjjjj@users.noreply.github.com>
2024-06-04 02:16:06 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -34,7 +34,7 @@ async function getPoses() {
 }
 
 async function setup() {
-  createCanvas(640, 480);
+  createCanvas(windowWidth, windowHeight);
   video = createCapture(VIDEO, videoReady);
   video.size(width, height);
   video.hide();
@@ -59,6 +59,16 @@ function drawSkeleton() {
   for (let i = 0; i < poses.length; i++) {
     pose = poses[i];
     // shoulder to wrist
+    partA = pose.keypoints[0]
+if(partA.score > 0.1) {
+  //line(partA.x, partA.y, partB.x, partB.y)
+  push()
+    textSize(40)
+    scale(-1,1)
+    text("412730649 吳羽婕",partA.x-width, paartA.y-250)
+      //print(partA.x)
+  pop()
+}
     for (j = 5; j < 9; j++) {
       if (pose.keypoints[j].score > 0.1 && pose.keypoints[j + 2].score > 0.1) {
         partA = pose.keypoints[j];
@@ -66,13 +76,23 @@ function drawSkeleton() {
         line(partA.x, partA.y, partB.x, partB.y);
       }
     }
+
+    function preload(){
+	    dogImg = loadImage("dog.gif");
+    }
+    
     // shoulder to shoulder
     partA = pose.keypoints[5];
     partB = pose.keypoints[6];
     if (partA.score > 0.1 && partB.score > 0.1) {
-      line(partA.x, partA.y, partB.x, partB.y);
-      
+      //line(partA.x, partA.y, partB.x, partB.y);
+      push()
+        image(dogImg,partA.x-25, partA.y-75,150,150)
+        image(dogImg,partB.x-25, partB.y-75,150,150)
+          // print(partA.x)
+      pop()
     }
+    
     // hip to hip
     partA = pose.keypoints[11];
     partB = pose.keypoints[12];
@@ -80,6 +100,7 @@ function drawSkeleton() {
       line(partA.x, partA.y, partB.x, partB.y);
       
     }
+    
     // shoulders to hips
     partA = pose.keypoints[5];
     partB = pose.keypoints[11];
@@ -93,6 +114,7 @@ function drawSkeleton() {
       line(partA.x, partA.y, partB.x, partB.y);
       
     }
+    
     // hip to foot
     for (j = 11; j < 15; j++) {
       if (pose.keypoints[j].score > 0.1 && pose.keypoints[j + 2].score > 0.1) {
